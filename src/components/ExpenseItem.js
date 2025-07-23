42ca3736c9f0e39836980c5613dbfac29b3bcdbb
karpovvicz <karpovvicz@gmail.com>
2025-07-23 12:26:39 +0200

    add props functionality and styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,17 @@
-function ExpenseItem() {
+import './ExpenseItem.css'
+
+function ExpenseItem(props) {
+
     return (
-        <div>
-            <h2>Expense item!</h2>
+        <>
+        <div className="expense-item">
+            <div>{props.date.toISOString()}</div>
+<div className="expense-item__description">
+    <h2>{props.title}</h2>
+    <div className="expense-item_price">{props.amount}</div>
+</div>
         </div>
+        </>
     )
 }
 
